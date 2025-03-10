2e758c8aa4381b13570317c5bf99bfe2e5f3d016
Cleidson Franklin <cleidsonads.dev@gmail.com>
2025-03-09 23:25:56 -0300

    Correção vite.config

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'lucide-react': '/node_modules/lucide-react' // Alias para o módulo
+    }
+  },
   build: {
     rollupOptions: {
       external: ['lucide-react']
     }
   }
-})
+});
