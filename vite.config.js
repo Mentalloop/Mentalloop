b41acc22450572a5cc20cee9ea9dcb79a39163b9
Cleidson Franklin <cleidsonads.dev@gmail.com>
2025-03-11 21:35:04 -0300

    Removendo a pasta front-end

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Diretório de saída para os arquivos de produção
+  },
 });
